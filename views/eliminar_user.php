8ca61eccff96a7db53d7c28a17edb7f4bd57741f
luiseduardodelacruz <150294913+luiseduardodelacruz@users.noreply.github.com>
2024-04-08 15:11:01 -0600

    Update eliminar_user.php

--- views/eliminar_user.php
+++ views/eliminar_user.php
@@ -1,21 +1,15 @@
 <?php
-
 session_start();
 error_reporting(0);
 
 $validar = $_SESSION['nombre'];
 
-if( $validar == null || $validar = ''){
+if ($validar == null || $validar == '') {
     header("Location: ../includes/login.php");
     die();
-
 }
-
-
-
-
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,27 +20,24 @@ if( $validar == null || $validar = ''){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    
     <div class="container mt-5">
-    <div class="row">
-    <div class="col-sm-6 offset-sm-3">
-    <div class="alert alert-danger text-center">
-    <p>¿Desea confirmar la eliminacion del registro?</p>
-    </div>
-
-    <div class="row">
-        <div class="col-sm-6">
-            <form action="../includes/_functions.php" method="POST">
-                <input type="hidden" name="accion" value="eliminar_registro">
-                <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
-                <input type="submit" name="" value="Eliminar" class= " btn btn-danger">
-                <a href="user.php" class="btn btn-success">Cancelar</a>
-
-                               
+        <div class="row">
+            <div class="col-sm-6 offset-sm-3">
+                <div class="alert alert-danger text-center">
+                    <p>¿Desea confirmar la eliminación del registro?</p>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <form action="../includes/_functions.php" method="POST">
+                            <input type="hidden" name="accion" value="eliminar_registro">
+                            <input type="hidden" name="id" value="<?php echo htmlspecialchars($_GET['id']); ?>">
+                            <input type="submit" value="Eliminar" class="btn btn-danger">
+                            <a href="user.php" class="btn btn-success">Cancelar</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-
-
 </body>
 </html>
