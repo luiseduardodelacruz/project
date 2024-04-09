e06de05bf198c786cdc82d22e468cf5a88189eee
luiseduardodelacruz <150294913+luiseduardodelacruz@users.noreply.github.com>
2024-04-08 18:12:19 -0600

    Update _db.php

--- includes/_db.php
+++ includes/_db.php
@@ -1,8 +1,18 @@
+
 <?php
-require_once("config.php");
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "r_user";
+
 
 $conexion = mysqli_connect($host, $user, $password, $database);
 if(!$conexion){
-    echo "No se realizo la conexion a la basa de datos, el error fue: " . mysqli_connect_error();
+echo "No se realizo la conexion a la basa de datos, el error fue:".
+mysqli_connect_error() ;
+
+
 }
+
 ?>
